03194ccbae12d27448f062cb880348be22b67ef7
Manjeetdhimaan <80199360+Manjeetdhimaan@users.noreply.github.com>
2021-07-30 18:24:20 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import classes from './App.css'
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder'
+ class  App extends Component{
+  render(){
+    return(
+    <div className={classes.App}>
+      <Layout><BurgerBuilder /></Layout>
+      
+    </div>
+  )}
+    
+}
+
+export default App;
